6fc34e653378d3fd21e002e5717c88372321a394
Ye Yint Aung <yeyint.23@ichat.sp.edu.sg>
2024-05-16 12:32:33 +0800

    Test

---
+++ JPRG-CA1/src/CA1/Student.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package CA1;
+
+/**
+ *
+ * @author yeyin
+ */
+public class Student {
+    this.student_id = 1;
+}
